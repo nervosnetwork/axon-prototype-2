ee802eec20b6dd0203e9daacc51b5caeb042b6f4
Hanssen <handsome0hell@gmail.com>
2021-05-24 15:20:44 +0800

    feat: add base models (#8)
    * build(contracts): init
    
    * build(contracts): dependencies for checking signature
    
    [ckb-lib-secp256k1](https://github.com/jjyr/ckb-dynamic-loading-secp256k1/tree/c9ae6a1d60f42c1905455739ec510971dd727b08/ckb-lib-secp256k1)
    [ckb-miscellaneous-scripts](https://github.com/nervosnetwork/ckb-miscellaneous-scripts)
    [ckb-system-scripts](https://github.com/nervosnetwork/ckb-system-scripts)
    
    * refactor(contracts): checking signature by Secp256k1 library
    
    * refactor(contracts): schemas library
    
    Basic schema types
    
    * refactor(contracts): common test helpers
    
    [ckb-dynamic-loading-secp256k1](https://github.com/jjyr/ckb-dynamic-loading-secp256k1/tree/c9ae6a1d60f42c1905455739ec510971dd727b08/tests)

---
+++ axon-ckb-contracts/tests/src/test_always_success.rs
@@ -0,0 +1,39 @@
+use super::Loader;
+use crate::common::*;
+use ckb_testtool::context::Context;
+use ckb_tool::ckb_types::{bytes::Bytes, core::TransactionBuilder, packed::*, prelude::*};
+
+const MAX_CYCLES: u64 = 10_000_000;
+
+#[test]
+fn test_success() {
+    // deploy contract
+    let mut context = Context::default();
+    let contract_bin: Bytes = Loader::default().load_binary("always-success");
+    let out_point = context.deploy_cell(contract_bin);
+
+    // prepare scripts
+    let lock_script = context
+        .build_script(&out_point, Bytes::from(vec![]))
+        .expect("script");
+    let lock_script_dep = CellDep::new_builder().out_point(out_point).build();
+
+    // prepare cells
+    let input_outpoint = context.create_cell(new_cell_output(1000, &lock_script), Bytes::new());
+    let input = CellInput::new_builder()
+        .previous_output(input_outpoint)
+        .build();
+
+    // build transaction
+    let tx = TransactionBuilder::default()
+        .input(input)
+        .cell_dep(lock_script_dep)
+        .build();
+    let tx = context.complete_tx(tx);
+
+    // run
+    let cycles = context
+        .verify_tx(&tx, MAX_CYCLES)
+        .expect("pass verification");
+    println!("consume cycles: {}", cycles);
+}
