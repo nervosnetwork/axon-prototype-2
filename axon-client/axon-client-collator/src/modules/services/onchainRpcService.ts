5b9245f6dd6f1243c7d542f51f4156e4c5ed0272
CQUPTYHM <yaohuiming789@gmail.com>
2021-07-25 21:05:49 +0800

    test: unit tests for onchainRpcService

--- axon-client/axon-client-collator/src/modules/services/onchainRpcService.ts
+++ axon-client/axon-client-collator/src/modules/services/onchainRpcService.ts
@@ -1,8 +1,8 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { modules } from "../../container";
 import Rpc from "@nervosnetwork/ckb-sdk-rpc";
 import JSONbig from "json-bigint";
 import { logger } from "axon-client-common/src/utils/logger";
-import { CKB_NODE_URL } from "axon-client-common/src/utils/environment";
 import RpcService from "./rpcService";
 
 @injectable()
@@ -11,16 +11,16 @@ export default class OnchainRpcService implements RpcService {
 
   // @ts-expect-error Unused
   // istanbul ignore next
-  #info = (msg: string) => {
+  private info = (msg: string) => {
     logger.info(`RpcService: ${msg}`);
   };
   // istanbul ignore next
-  #error = (msg: string) => {
+  private error = (msg: string) => {
     logger.error(`RpcService: ${msg}`);
   };
 
-  constructor() {
-    this.#client = new Rpc(CKB_NODE_URL);
+  constructor(@inject(modules.CKBRpc) client: Rpc) {
+    this.#client = client;
   }
 
   sendTransaction = async (rawTx: CKBComponents.RawTransaction): Promise<boolean> => {
@@ -29,24 +29,26 @@ export default class OnchainRpcService implements RpcService {
       await this.#client.sendTransaction(rawTx);
       return true;
     } catch (e) {
-      this.#error("sendTransaction error: " + e);
-      this.#error("rawTx: " + JSONbig.stringify(rawTx, null, 2));
+      this.error("sendTransaction error: " + e);
+      this.error("rawTx: " + JSONbig.stringify(rawTx, null, 2));
       return false;
     }
   };
 
   // @ts-expect-error Unused
-  private toArrayBuffer = (buf: Buffer): ArrayBuffer => {
+  // istanbul ignore next
+  private toArrayBuffer(buf: Buffer): ArrayBuffer {
     const ab = new ArrayBuffer(buf.length);
     const view = new Uint8Array(ab);
     for (let i = 0; i < buf.length; ++i) {
       view[i] = buf[i];
     }
     return ab;
-  };
+  }
 
   // @ts-expect-error Unused
-  private toBuffer = (arrayBuffer: ArrayBuffer): Buffer => {
+  // istanbul ignore next
+  private toBuffer(arrayBuffer: ArrayBuffer): Buffer {
     const b = Buffer.alloc(arrayBuffer.byteLength);
     const view = new Uint8Array(arrayBuffer);
 
@@ -54,5 +56,5 @@ export default class OnchainRpcService implements RpcService {
       b[i] = view[i];
     }
     return b;
-  };
+  }
 }
