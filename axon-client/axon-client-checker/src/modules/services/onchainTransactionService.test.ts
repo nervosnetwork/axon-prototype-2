6865eebf48cf5cd9d70c2ca92bf254425636426b
handsome0hell <handsome0hell@gmail.com>
2021-07-27 19:38:48 +0800

    test: unit tests for `onchainTransactionService`

---
+++ axon-client/axon-client-checker/src/modules/services/onchainTransactionService.test.ts
@@ -0,0 +1,94 @@
+import "reflect-metadata";
+
+import CKB from "@nervosnetwork/ckb-sdk-core";
+
+import TransactionService from "./transactionService";
+import OnchainTransactionService from "./onchainTransactionService";
+
+import { Transformation } from "axon-client-common/src/modules/models/transformation/interfaces/transformation";
+import { CELL_DEPS, SELF_PRIVATE_KEY } from "axon-client-common/src/utils/environment";
+
+import { createMock } from "ts-auto-mock";
+
+type StructuredWitness = CKBComponents.WitnessArgs | CKBComponents.Witness;
+type SignatureProvider = string | ((message: string | Uint8Array) => string);
+type LockHash = string;
+
+class Context {
+  ckb: CKB;
+  transactionService: TransactionService;
+
+  constructor(ckb: CKB, transactionService: TransactionService) {
+    this.ckb = ckb;
+    this.transactionService = transactionService;
+  }
+}
+
+function prepareContext(): Context {
+  const mockCKB: CKB = createMock<CKB>();
+  mockCKB.signWitnesses = jest.fn((_: SignatureProvider | Map<LockHash, SignatureProvider>) =>
+    jest.fn((_: { transactionHash: string; witnesses: StructuredWitness[] }): StructuredWitness[] => {
+      return [{ lock: "0x", inputType: "0x", outputType: "0x" }, "0x"];
+    }),
+  );
+
+  const transactionService = new OnchainTransactionService(mockCKB);
+
+  return new Context(mockCKB, transactionService);
+}
+
+function isComposedTransactionSuccess(mockTransformation: Transformation, context: Context) {
+  const mockCKB = context.ckb;
+
+  const inputs = mockTransformation.toCellInput();
+  const rawTx: CKBComponents.RawTransaction = {
+    version: "0x0",
+    headerDeps: [],
+    cellDeps: mockTransformation.toCellDeps().concat(CELL_DEPS),
+    inputs,
+    witnesses: new Array(inputs.length).fill("0x"),
+    outputs: mockTransformation.toCellOutput(),
+    outputsData: mockTransformation.toCellOutputData(),
+  };
+
+  const rawTransactionToHash = mockCKB.utils.rawTransactionToHash as jest.Mock;
+  expect(rawTransactionToHash).toHaveBeenCalledTimes(1);
+  expect(rawTransactionToHash.mock.calls[0][0]).toEqual(rawTx);
+  expect(rawTransactionToHash).toHaveReturnedWith(mockTransformation.composedTxHash);
+
+  const signWitnesses = mockCKB.signWitnesses as jest.Mock;
+  expect(signWitnesses).toHaveBeenCalledTimes(1);
+  expect(signWitnesses).toHaveBeenCalledWith(SELF_PRIVATE_KEY);
+
+  const signWitnessesWithKey = signWitnesses.mock.results[0].value;
+  expect(signWitnessesWithKey).toHaveBeenCalledTimes(1);
+  expect(signWitnessesWithKey.mock.calls[0][0]).toEqual({
+    transactionHash: mockTransformation.composedTxHash,
+    witnesses: mockTransformation.toWitness(),
+  });
+
+  rawTx.witnesses = ["0x10000000100000001000000010000000", "0x"];
+  expect(mockTransformation.composedTx).toEqual(rawTx);
+}
+
+describe("OnchainTransactionService", () => {
+  test("composeTransaction should failed if xfer.skip is true", async () => {
+    const context = prepareContext();
+
+    const mockTransformation = createMock<Transformation>();
+    mockTransformation.skip = true;
+
+    await context.transactionService.composeTransaction(mockTransformation);
+
+    expect(() => isComposedTransactionSuccess(mockTransformation, context)).toThrow();
+  });
+
+  test("composeTransaction should success", async () => {
+    const context = prepareContext();
+
+    const mockTransformation = createMock<Transformation>();
+    await context.transactionService.composeTransaction(mockTransformation);
+
+    expect(() => isComposedTransactionSuccess(mockTransformation, context)).not.toThrow();
+  });
+});
--- axon-client/axon-client-checker/src/modules/services/onchainTransactionService.ts
+++ axon-client/axon-client-checker/src/modules/services/onchainTransactionService.ts
@@ -1,8 +1,10 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { modules } from "../../container";
 import CKB from "@nervosnetwork/ckb-sdk-core";
+import { serializeWitnessArgs } from "@nervosnetwork/ckb-sdk-utils";
 import JSONbig from "json-bigint";
 import { logger } from "axon-client-common/src/utils/logger";
-import { CELL_DEPS, CKB_NODE_URL, SELF_PRIVATE_KEY } from "axon-client-common/src/utils/environment";
+import { CELL_DEPS, SELF_PRIVATE_KEY } from "axon-client-common/src/utils/environment";
 import { Transformation } from "axon-client-common/src/modules/models/transformation/interfaces/transformation";
 import TransactionService from "./transactionService";
 
@@ -11,21 +13,23 @@ this service compose tx for rpc
  */
 @injectable()
 export default class OnchainTransactionService implements TransactionService {
-  readonly #ckb: CKB;
+  private readonly _ckb: CKB;
 
-  #info = (msg: string) => {
+  private info(msg: string): void {
     logger.info(`TransactionService: ${msg}`);
-  };
-  // @ts-expect-error Unused
-  #error = (msg: string) => {
-    logger.error(`TransactionService: ${msg}`);
-  };
-
-  constructor() {
-    this.#ckb = new CKB(CKB_NODE_URL);
   }
 
-  composeTransaction = (xfer: Transformation) => {
+  // @ts-expect-error Unused
+  // istanbul ignore next
+  private error(msg: string): void {
+    logger.error(`TransactionService: ${msg}`);
+  }
+
+  constructor(@inject(modules.CKBCKB) ckb: CKB) {
+    this._ckb = ckb;
+  }
+
+  composeTransaction(xfer: Transformation): void {
     if (xfer.skip) {
       return;
     }
@@ -43,56 +47,63 @@ export default class OnchainTransactionService implements TransactionService {
     witness.push(...xfer.toWitness());
 
     // the secp sig shoul be signed at input-0
-    const [signedTx, txHash] = this.#composeTxAndSign(deps, inputs, outputs, outputsData, witness);
+    const [signedTx, txHash] = this.composeTxAndSign(deps, inputs, outputs, outputsData, witness);
 
-    this.#info("composed tx: " + JSONbig.stringify(signedTx, null, 2));
-    this.#info("composed txHash: " + txHash);
+    this.info("composed tx: " + JSONbig.stringify(signedTx, null, 2));
+    this.info("composed txHash: " + txHash);
 
     xfer.composedTx = signedTx;
     xfer.composedTxHash = txHash;
 
     return;
-  };
+  }
 
-  #composeTxAndSign = (
+  private composeTxAndSign(
     deps: Array<CKBComponents.CellDep>,
     inputs: Array<CKBComponents.CellInput>,
     outputs: Array<CKBComponents.CellOutput>,
     outputsData: Array<string>,
     witness: Array<CKBComponents.WitnessArgs>,
-  ): [CKBComponents.RawTransaction, string] => {
+  ): [CKBComponents.RawTransaction, string] {
     deps.push(...CELL_DEPS);
 
     const rawTx: CKBComponents.RawTransaction = {
       version: "0x0",
       headerDeps: [],
       cellDeps: deps,
-      inputs: inputs,
+      inputs,
       witnesses: new Array(inputs.length).fill("0x"),
-      outputs: outputs,
-      outputsData: outputsData,
+      outputs,
+      outputsData,
     };
 
     const rawTxWithoutWitness: CKBComponents.RawTransactionToSign = rawTx;
 
-    const txHash = this.#ckb.utils.rawTransactionToHash(rawTxWithoutWitness);
+    const txHash = this._ckb.utils.rawTransactionToHash(rawTxWithoutWitness);
 
-    // const secp256k1Witness =  this.#ckb.signWitnesses(SELF_PRIVATE_KEY)({
+    // const secp256k1Witness =  this._ckb.signWitnesses(SELF_PRIVATE_KEY)({
     //     transactionHash: txHash,
     //     witnesses: [{ lock: '', inputType: '', outputType: '' }],
     // })[0]
 
     //this should be wrong cause we should tell the tool which slot to contain witness
-    const secp256k1Witness = this.#ckb.signWitnesses(SELF_PRIVATE_KEY)({
-      transactionHash: txHash,
-      witnesses: witness,
-    });
+    const secp256k1Witness = this._ckb
+      .signWitnesses(SELF_PRIVATE_KEY)({
+        transactionHash: txHash,
+        witnesses: witness,
+      })
+      .map((witness): string => {
+        if ((witness as CKBComponents.WitnessArgs).lock) {
+          return serializeWitnessArgs(witness as CKBComponents.WitnessArgs);
+        }
+        return witness as string;
+      });
 
-    const signedTx: any = {
+    const signedTx: CKBComponents.RawTransaction = {
       ...rawTx,
-      //witnesses: [witness, ...rawTransaction.witnesses.slice(1)],
       witnesses: secp256k1Witness,
     };
+
     return [signedTx, txHash];
-  };
+  }
 }
