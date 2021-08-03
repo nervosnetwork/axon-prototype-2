b778e8322f7aafa468bdccd37803fc9db1bec896
yhm <57828206+CQUPTYHM@users.noreply.github.com>
2021-08-03 17:43:29 +0800

    feat: schemas in molecule (#45)
    * feat: task cell schema
    
    * feat: checker info cell schema
    
    * feat: code cell schema
    
    * feat: sidechain fee cell schema
    
    * feat: sidechain state cell schema

--- axon-ckb-contracts/common-raw/src/molecule/cell/mod.rs
+++ axon-ckb-contracts/common-raw/src/molecule/cell/mod.rs
@@ -1,6 +1,11 @@
 pub mod checker_bond;
+pub mod checker_info;
+pub mod code;
 pub mod global_config;
 pub mod sidechain_bond;
 pub mod sidechain_config;
+pub mod sidechain_fee;
 pub mod sidechain_registry;
+pub mod sidechain_state;
 pub mod sudt_token;
+pub mod task;
