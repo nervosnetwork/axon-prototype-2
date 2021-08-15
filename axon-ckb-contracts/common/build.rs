20ba3b0906594753d03e066defffb4066397c951
LycrusHamster <liang2929@hotmail.com>
2021-08-15 23:29:05 +0800

    fix: pre-set env will be triggered via build.rs and add targets to Makefile

--- axon-ckb-contracts/common/build.rs
+++ axon-ckb-contracts/common/build.rs
@@ -1,11 +1,14 @@
 use std::env;
 use std::path::Path;
 
+const GCC_FLAG: &str = "gcc_typehash";
+
 /*
 capsule is not support to pass feature or cfg while compilation, thus this build.rs lives
  */
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed={}", GCC_FLAG);
 
     let p = env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR error?");
 
@@ -13,7 +16,7 @@ fn main() {
 
     //let a = read_file_path.as_path().to_str().unwrap().to_string();
 
-    let gcc_flag = env::var("gcc_typehash").unwrap_or("test_gcc".to_string());
+    let gcc_flag = env::var(GCC_FLAG).unwrap_or("test_gcc".to_string());
 
     let gcc_typehash = match &gcc_flag[..] {
         "test_gcc" => "test_gcc",
@@ -25,7 +28,10 @@ fn main() {
     };
 
     if gcc_typehash == "custom_gcc" && !read_file_path.exists() {
-        panic!("gcc_typehash=custom_gcc is set, but file 'global_config_type_hash' does not exist under 'common' package")
+        panic!(
+            "{}=custom_gcc is set, but file 'global_config_type_hash' does not exist under 'common' package",
+            GCC_FLAG
+        )
     }
 
     println!("cargo:rustc-cfg=gcc_typehash=\"{}\"", gcc_typehash);
